4ae5ff956fb41e32734d7eecd8390b96c975795f
Wiseman <25WisemanN@badinhs.org>
2025-02-28 13:12:50 -0500

    Psuedocode / General idea made

---
+++ RPSWiseman.js
@@ -0,0 +1,10 @@
+/*
+1. Get the input from the user, prolly through a prompt
+2. Use a random number generator to choose a computer respnoce
+3. Compare result of inputedd value to the chosen reponce  
+    maybe use the radom to activate of three loops that has the results in it
+4. Output who won and why in the console
+    Once again most likly inside of one f three functions based off the random number generator
+5. Make it work for three rounds
+*/
+
