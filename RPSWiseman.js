e9b66a13e29152d9a767e8c7c55a984b9276c16c
Wiseman <25WisemanN@badinhs.org>
2025-03-05 14:02:37 -0500

    Created the actual game code

--- RPSWiseman.js
+++ RPSWiseman.js
@@ -10,19 +10,48 @@
 
 let PlayerChoice = prompt('Rock, Paper, Scissors?', '')
 
-if (PlayerChoice != 'rock' || 'paper'){
-    prompt('That was not one of the options, reload the page and try again')
+PlayerChoice = PlayerChoice.toLowerCase()
+
+if(PlayerChoice != 'rock' && PlayerChoice != 'paper' && PlayerChoice != 'scissors'){
+    alert('That was not one of the options, reload the page and try again')
 }
 
 console.log(PlayerChoice)
 
 let ComChoice = Math.random() * 3
 console.log(ComChoice)
-
+console.log(PlayerChoice)
 if (ComChoice <= 1){
-    console.log(1)
+    console.log('Computer choose Rock')
+    if(PlayerChoice == ('rock')){
+        console.log('Tie, both choose Rock')
+    }
+    if(PlayerChoice == ('paper')){
+        console.log('User wins, Paper beats Rock')
+    }
+    if(PlayerChoice == ('scissors')){
+        console.log('Computer wins, Rock beats Scissors')
+    }
 }else if (ComChoice <= 2){
-    console.log(2)
+    console.log('Computer choose Paper')
+    if(PlayerChoice == ('paper')){
+        console.log('Tie, both choose Paper')
+    }
+    if(PlayerChoice == ('scissors')){
+        console.log('User wins, Scissors beats Paper')
+    }
+    if(PlayerChoice == ('rock')){
+        console.log('Computer wins, Paper beats Rock')
+    }
 }else if (ComChoice <= 3){
-    console.log(3)
+    console.log('Computer choose Scissors')
+    if(PlayerChoice == ('scissors')){
+        console.log('Tie, both choose Scissors')
+    }
+    if(PlayerChoice == ('rock')){
+        console.log('User wins, Rock beats Scissors')
+    }
+    if(PlayerChoice == ('paper')){
+        console.log('Computer wins, Scissors beats Paper')
+    }
 }
