7ece9d62a2f6fba10f5f56a9a73f4d590796383e
Wiseman <25WisemanN@badinhs.org>
2025-03-06 13:13:13 -0500

    Finalized code for first to 5 wins, also incldes deactivated code to become who gets the most wins in a specific round amount.

--- RPSWiseman.js
+++ RPSWiseman.js
@@ -8,15 +8,19 @@
 5. Make it work for three rounds
 */
 
+let PlayerWins = 0
+let ComputerWins = 0
+
+for(let RoundCount = 1; RoundCount <= 10; RoundCount++){
+
 let PlayerChoice = prompt('Rock, Paper, Scissors?', '')
 
 PlayerChoice = PlayerChoice.toLowerCase()
 
 if(PlayerChoice != 'rock' && PlayerChoice != 'paper' && PlayerChoice != 'scissors'){
     alert('That was not one of the options, reload the page and try again')
+    break
 }
-
-console.log(PlayerChoice)
 
 let ComChoice = Math.random() * 3
 console.log(ComChoice)
@@ -28,9 +32,11 @@ if (ComChoice <= 1){
     }
     if(PlayerChoice == ('paper')){
         console.log('User wins, Paper beats Rock')
+        PlayerWins ++
     }
     if(PlayerChoice == ('scissors')){
         console.log('Computer wins, Rock beats Scissors')
+        ComputerWins ++
     }
 }else if (ComChoice <= 2){
     console.log('Computer choose Paper')
@@ -39,9 +45,11 @@ if (ComChoice <= 1){
     }
     if(PlayerChoice == ('scissors')){
         console.log('User wins, Scissors beats Paper')
+        PlayerWins ++
     }
     if(PlayerChoice == ('rock')){
         console.log('Computer wins, Paper beats Rock')
+        ComputerWins ++
     }
 }else if (ComChoice <= 3){
     console.log('Computer choose Scissors')
@@ -50,8 +58,33 @@ if (ComChoice <= 1){
     }
     if(PlayerChoice == ('rock')){
         console.log('User wins, Rock beats Scissors')
+        PlayerWins ++
     }
     if(PlayerChoice == ('paper')){
         console.log('Computer wins, Scissors beats Paper')
+        ComputerWins ++
     }
 }
+
+    if(ComputerWins == 5){
+        alert('Computer wins!!')
+        break
+    }
+
+    if (PlayerWins == 5){
+        alert('User Wins!!')
+        break
+    }
+}
+//Code for going by rounds rather than first to a win ammount
+// if(PlayerWins > ComputerWins){
+//     alert(`User wins with ${PlayerWins} wins! `)
+// }
+
+// if(ComputerWins > PlayerWins){
+//     alert(`Computer wins with ${ComputerWins} wins!`)
+// }
+
+// if(ComputerWins == PlayerWins){
+//     alert(`Tie, both have ${ComputerWins} wins`)
+// }
